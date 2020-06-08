7a1e9b2a47a8bac831bc445ff996473ddc64c588
spiderohara <spiderohara44@gmail.com>
2020-06-08 15:39:43 +0530

    Checking Global .gitignore

--- src/com/spiderohara/coding/interviewbit/level2/array/MinStepsInInfiniteGrid.java
+++ src/com/spiderohara/coding/interviewbit/level2/array/MinStepsInInfiniteGrid.java
@@ -1,5 +1,31 @@
 package com.spiderohara.coding.interviewbit.level2.array;
 
-public class MinStepsInInfiniteGrid {
+import java.util.ArrayList;
 
+public class MinStepsInInfiniteGrid {
+	public int coverPoints(ArrayList<Integer> A, ArrayList<Integer> B) {
+		int i = 0;
+		int count = A.size();
+		int steps = 0;
+		for (i = 0; i < count - 1; i++) {
+			int xStep = A.get(i) - A.get(i + 1);
+			if (xStep < 0) {
+				xStep = xStep * -1;
+			}
+
+			int yStep = B.get(i) - B.get(i + 1);
+			if (yStep < 0) {
+				yStep = yStep * -1;
+			}
+
+			if (xStep >= yStep) {
+				steps += xStep;
+			} else {
+				steps += yStep;
+			}
+
+		}
+
+		return steps;
+	}
 }
--- src/com/spiderohara/coding/startup/Startup.java
+++ src/com/spiderohara/coding/startup/Startup.java
@@ -1,7 +1,23 @@
 package com.spiderohara.coding.startup;
 
+import java.util.ArrayList;
+
+import com.spiderohara.coding.interviewbit.level2.array.MinStepsInInfiniteGrid;
+
 public class Startup {
 	public static void main(String[] args) {
-		System.out.println("Hello, World.");
+		ArrayList<Integer> A = new ArrayList<>();
+		A.add(0);
+		A.add(1);
+		A.add(1);
+
+		ArrayList<Integer> B = new ArrayList<>();
+		B.add(0);
+		B.add(1);
+		B.add(2);
+
+		MinStepsInInfiniteGrid minStepsInInfiniteGrid = new MinStepsInInfiniteGrid();
+		int coverPoints = minStepsInInfiniteGrid.coverPoints(A, B);
+		System.out.println(coverPoints);
 	}
 }
